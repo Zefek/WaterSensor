20e78bfc75f4a3b1160586ffe2a4abfcb2eec851
Zefek <petr.sponky@gmail.com>
2025-03-31 08:54:37 +0200

    Úpravy měření

--- WaterSensor.ino
+++ WaterSensor.ino
@@ -1,36 +1,80 @@
-int count = 0;
-int lastValue = HIGH;
+#include <EspDrv.h>
+#include <MQTTClient.h>
+#include <SoftwareSerial.h>
+#include "config.h"
 
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(2, INPUT_PULLUP);
-  Serial.begin(9600);
-  lastValue = digitalRead(2);
-  attachInterrupt(digitalPinToInterrupt(2), Add, CHANGE);
-}
+void MQTTMessageReceive(char* topic, uint8_t* payload, uint16_t length) { }
+MQTTConnectData mqttConnectData = { MQTTHost, 1883, "WaterMeter", MQTTUsername, MQTTPassword, "", 0, false, "", false, 0x0F }; 
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+SoftwareSerial serial(4, 5);
+EspDrv espDrv(&serial);
+MQTTClient mqttClient(&espDrv, MQTTMessageReceive);
+char data[32];
+unsigned long lastSendToMQTT = 0;
 
+unsigned long lastRissing = 0;
+unsigned long count = 0; 
 
 void Add()
 {
-  int value = digitalRead(2);
-  if(lastValue != value )
+  if(millis() - lastRissing > 84)
   {
-    if(value == LOW)
-    {
-      count++;
-    }
-    lastValue = value;
+    count++;
+    lastRissing = millis();
   }
-  char buffer[10];
-  sprintf(buffer, "%.2f", count/10);
-  Serial.print("Variable 1:");
-  Serial.print(value);
-  Serial.print(",Variable 2:");
-  Serial.print(count);
-  Serial.print(",Variable 3:");
-  Serial.println(buffer);
+}
+
+bool Connect()
+{
+  int wifiStatus = espDrv.GetConnectionStatus();
+  bool wifiConnected = wifiStatus == WL_CONNECTED;
+  if(wifiStatus == WL_DISCONNECTED || wifiStatus == WL_IDLE_STATUS)
+  {
+    wifiConnected = espDrv.Connect(WifiSSID, WifiPassword);
+  }
+  if(wifiConnected)
+  {
+
+    bool isConnected = mqttClient.IsConnected();
+    if(!isConnected)
+    {
+      return mqttClient.Connect(mqttConnectData);
+    }
+    else
+    {
+      return true;
+    }
+  }
+  return false;
+}
+
+void setup() 
+{
+  pinMode(2, INPUT);
+  Serial.begin(57600);
+  serial.begin(57600);
+  espDrv.Init(16);
+  espDrv.Connect(WifiSSID, WifiPassword);
+  attachInterrupt(digitalPinToInterrupt(2), Add, RISING);
+}
+
+void loop() 
+{
+  mqttClient.Loop();
+  unsigned long currentMillis = millis();
+  if(currentMillis - lastSendToMQTT >= 300000)
+  {    
+    detachInterrupt(digitalPinToInterrupt(2));
+    Serial.print("Variable 1:");
+    Serial.println(count);
+    if(Connect())
+    {
+      mqttClient.Publish(WATERCONSUMPTION, 0);
+      mqttClient.Publish(WATERCONSUMPTION, count);
+      mqttClient.Disconnect();
+      count = 0;
+    }
+    lastSendToMQTT = currentMillis;
+    attachInterrupt(digitalPinToInterrupt(2), Add, RISING);
+  }
 }
