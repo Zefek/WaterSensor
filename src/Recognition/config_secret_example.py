986b9f0fe5c2a829ab64989b470525d4d24647f7
Zefek <petr.sponky@gmail.com>
2026-05-24 10:02:21 +0200

    Detekce

---
+++ src/Recognition/config_secret_example.py
@@ -0,0 +1,9 @@
+# Zkopíruj tento soubor jako config_secret.py a vyplň skutečné hodnoty.
+# config_secret.py je v .gitignore - nikdy ho necommituj.
+
+RABBITMQ_HOST = "rabbitmq-host"
+RABBITMQ_PORT = 5672
+RABBITMQ_VHOST = "/"
+RABBITMQ_USER = "your-username"
+RABBITMQ_PASSWORD = "your-password"
+RABBITMQ_HEARTBEAT = 60
