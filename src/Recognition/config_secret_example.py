e01a55a6f71750b56437ee89002803ca97fad67e
Zefek <petr.sponky@gmail.com>
2026-07-27 12:05:03 +0200

    Vylepšení

--- src/Recognition/config_secret_example.py
+++ src/Recognition/config_secret_example.py
@@ -2,8 +2,12 @@
 # config_secret.py je v .gitignore - nikdy ho necommituj.
 
 RABBITMQ_HOST = "rabbitmq-host"
-RABBITMQ_PORT = 5672
+RABBITMQ_PORT = 5671
 RABBITMQ_VHOST = "/"
 RABBITMQ_USER = "your-username"
 RABBITMQ_PASSWORD = "your-password"
 RABBITMQ_HEARTBEAT = 60
+
+RABBITMQ_USE_TLS = True
+RABBITMQ_TLS_CA_FILE = None
+RABBITMQ_TLS_SERVER_NAME = None
